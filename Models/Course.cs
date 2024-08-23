6733617f38a655324ebac7dd380c399a2529eca5
IvanDemkin <zaphta@gmail.com>
2024-08-23 13:56:49 +0700

    Add project files.

---
+++ Models/Course.cs
@@ -0,0 +1,14 @@
+﻿namespace EFProj.Models
+{
+    //[Table("courses")]
+    public class Course
+    {
+        //[Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        //[Column("Length")]
+        public int Duration { get; set; }
+        //[NotMapped]
+        public string Description { get; set; }
+    }
+}
